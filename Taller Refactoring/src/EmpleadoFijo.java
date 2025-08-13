54edd7bf09bff6d9e6c288c9a15ab4c7fcff5f4c
CesarD25 <108200197+CesarD25@users.noreply.github.com>
2025-08-13 14:27:18 -0500

    Dealing with generalization
    Se aplicaron 4 tecnicas de refactorizacion

--- Taller Refactoring/src/EmpleadoFijo.java
+++ Taller Refactoring/src/EmpleadoFijo.java
@@ -3,19 +3,17 @@ public class EmpleadoFijo extends Empleado {
     private String genero;
 
     public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, Departamento departamento, double bonoAnual, String genero) {
-        super(nombre, salarioBase, horasTrabajadas, 0, departamento);
+        super(nombre, salarioBase, horasTrabajadas, departamento);
         this.bonoAnual = bonoAnual;
         this.genero = genero;
     }
 
+    @Override
     public void imprimirDetalles() {
-        System.out.println("Nombre: " + super.getNombre());
-        System.out.println("Genero: " + super.getNombre());
-        System.out.println("Salario: " + super.getSalarioBase());
-        System.out.println("Horas trabajadas: " + super.getHorasTrabajadas());
-        System.out.println("Departamento: " + super.getDepartamento());
-        System.out.println("bonoAnual: " + bonoAnual);
+        super.imprimirDetalles();
+        System.out.println("Bono anual: " + bonoAnual); 
     }
+
 
     // Más metodos
 }
